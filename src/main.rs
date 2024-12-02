382418ca2094e57bd37e2e021dee3537caeeb4ba
breakfast <qwertyytheartist@gmail.com>
2024-12-01 20:52:35 -0800

    basic frame with background

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let (width, height) = termion::terminal_size().unwrap();
+    print!("\x1b[48;2;230;240;255m");
+    for y in 0..height {
+        for x in 0..width {
+            print!(" ");
+        }
+        println!();
+    }
 }
